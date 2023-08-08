ea32baba6898a57c0b808c0c23c876473b06276a
BenBarrett89 <benjaminbarrett1989@gmail.com>
2023-08-08 22:56:04 +0100

    Refactor more styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl font-bold">Hello World</h1>
-      <div className="py-8">
-        <button
-          className="bg-gradient-to-b from-purple-600 to-blue-600 hover:from-purple-900 hover:to-blue-900 px-10 py-4 rounded-xl"
-          onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div>
-        <p className="py-2">
-          <a href="/#/somewhere">Link to somewhere</a>
-        </p>
-        <p className="py-2">
-          <a href="/#/other">Link to other</a>
-        </p>
+      <div className="flex flex-col justify-center mt-10 items-center text-center p-9">
+        <h1 className="text-6xl font-bold">Hello World</h1>
+        <div className="py-8">
+          <button
+            className="bg-gradient-to-b from-purple-600 to-blue-600 hover:from-purple-900 hover:to-blue-900 px-10 py-4 rounded-xl"
+            onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+        <div>
+          <p className="py-2">
+            <a href="/#/somewhere">Link to somewhere</a>
+          </p>
+          <p className="py-2">
+            <a href="/#/other">Link to other</a>
+          </p>
+        </div>
       </div>
     </>
   )
